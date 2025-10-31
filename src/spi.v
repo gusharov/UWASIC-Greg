db3b841c7c59eaf2d2a1d5fd702453aacd61f92d
Gregory-Usharov <gregusharov@gmail.com>
2025-10-31 13:06:39 -0400

    forgot to clock it properly

--- src/spi.v
+++ src/spi.v
@@ -31,7 +31,7 @@ dflop clk1(.D(sclk),.clk(clk),.Q(synclock1),.rst_n(rst_n));
 dflop clk2(.D(synclock1),.clk(clk),.Q(synclock2),.rst_n(rst_n)); 
 dflop d1(.D(sdi),.clk(clk),.Q(da1),.rst_n(rst_n));
 dflop d2(.D(da1),.clk(clk),.Q(da2),.rst_n(rst_n)); 
-sclkdflop d3(.D(da2),.clk(clk),.Q(da3),.rst_n(rst_n), .en(synclock1));
+sclkdflop d3(.D(da2),.clk(clk),.Q(da3),.rst_n(rst_n), .en(synclock1), .en2(synclock2));
 dflop cs1(.D(cs),.clk(clk),.Q(syncs1),.rst_n(rst_n));
 dflop cs2(.D(syncs1),.clk(clk),.Q(syncs2),.rst_n(rst_n)); 
 
@@ -128,6 +128,7 @@ endmodule
 module sclkdflop (
     input  D,     
     input en,
+    input en2,
     input clk,
     input rst_n,    
     output reg Q
@@ -136,7 +137,7 @@ always @(posedge clk or negedge rst_n) begin
     if(!rst_n) begin
         Q <= 1'b0;
     end
-    else if(en) begin
+    else if(en == 0 && en2 == 1) begin
         Q <= D;
     end
 end
